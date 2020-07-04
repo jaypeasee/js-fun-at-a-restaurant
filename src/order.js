99369dd813c2e5f55b5e00b0acd974de1d6918c8
JP Carey <jpcarey4@gmail.com>
2020-07-04 16:05:11 -0600

    Complete order test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,39 @@
+function takeOrder(orderNumber, orderArray) {
+  if (orderArray.length <= 2) {
+    orderArray.push(orderNumber);
+    return orderArray;
+  }
+};
+
+function refundOrder(orderNumber, orderArray) {
+  orderArray.shift();
+  return orderArray;
+};
+
+function listItems(itemsArr) {
+  var orderItems = "";
+  var newOrderItems;
+  for (var i = 0; i < itemsArr.length; i++) {
+    orderItems += `${itemsArr[i].item}, `
+  }
+  newOrderItems = orderItems.substring(0, orderItems.length - 2);
+  return newOrderItems;
+}
+
+function searchOrder(ordersArr, orderChecker) {
+  var orderPresent = false;
+  for (var i = 0; i < ordersArr.length; i++) {
+    if (orderChecker === ordersArr[i].item) {
+      orderPresent = true;
+    }
+  }
+    console.log(orderPresent);
+    return orderPresent;
+  };
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
