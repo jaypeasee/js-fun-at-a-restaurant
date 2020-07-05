ccd3580cfcf2a9fd0c835fb42098306de08018b7
JP Carey <jpcarey4@gmail.com>
2020-07-05 15:39:07 -0600

    Complete restaurant test

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,56 @@
+function createRestaurant(name, menus, breakfast, lunch, dinner) {
+  var menus = {
+    breakfast: [],
+    lunch: [],
+    dinner: [],
+  }
+  var pizzaRestaurant = {
+    name: name,
+    menus: menus,
+  }
+  return pizzaRestaurant;
+};
+
+function addMenuItem(restaurant, newItem) {
+  var isUnique = true;
+  var restKey = Object.keys(restaurant.menus)
+  for (var j = 0; j < restKey.length; j++) {
+    var menu = restaurant.menus[restKey[j]];
+    for (var i = 0; i < menu.length; i++) {
+      if (newItem === menu[i]) {
+        isUnique = false;
+      }
+    }
+  }
+  if (isUnique) {
+    if (newItem.type === "breakfast") {
+      restaurant.menus.breakfast.push(newItem);
+    } else if (newItem.type === "lunch") {
+      restaurant.menus.lunch.push(newItem);
+    } else if (newItem.type === "dinner") {
+      restaurant.menus.dinner.push(newItem);
+    }
+  }
+
+  return restaurant;
+};
+
+
+function removeMenuItem(restaurant, newItem, menuType) { 
+if (menuType === "breakfast") {
+  restaurant.menus.breakfast.pop();
+  return `No one is eating our ${newItem} - it has been removed from the breakfast menu!`;
+ }  else if (menuType === "dinner") {
+  restaurant.menus.dinner.pop();
+  return `No one is eating our ${newItem} - it has been removed from the dinner menu!`
+} else if (newItem.includes(newItem) == true) {
+  return `Sorry, we don't sell ${newItem}, try adding a new recipe!`;
+}
+ } ;
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
